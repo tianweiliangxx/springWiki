6838ffafeb4fc1e9128c7add3f216f2d59b7eb81
闫海亮 <1015951447@qq.com>
2021-03-23 15:31:16 +0800

    2-5 增加logback配置文件

--- src/main/java/com/hailiang/wiki/WikiApplication.java
+++ src/main/java/com/hailiang/wiki/WikiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WikiApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(WikiApplication.class, args);
     }
