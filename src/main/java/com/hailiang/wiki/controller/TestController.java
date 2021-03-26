c52f3bdc80ae6c88253ea9c2270a13ed41d41f03
闫海亮 <1015951447@qq.com>
2021-03-26 15:22:00 +0800

    2-7 增加Hello接口测试test.http

--- src/main/java/com/hailiang/wiki/controller/TestController.java
+++ src/main/java/com/hailiang/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.hailiang.wiki.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +12,13 @@ public class TestController {
      * @return
      */
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World Post-" + name;
     }
 }
