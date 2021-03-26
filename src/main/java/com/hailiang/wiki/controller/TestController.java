6255b7a698f63c6f5442c96c4445c2853ca5e81a
闫海亮 <1015951447@qq.com>
2021-03-26 16:11:14 +0800

    2-8 增加自定义配置示例

--- src/main/java/com/hailiang/wiki/controller/TestController.java
+++ src/main/java/com/hailiang/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.hailiang.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,14 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.name: TEST}")
+    private String testName;
+
     /**
      * resful是一种风格 ：/hello/1 === /hello?id=1
      * @return
      */
-
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return "Hello World" + testName;
     }
 
     @PostMapping("/hello/post")
