a91334e141eba83576545361c0605c3e3a197cb3
闫海亮 <1015951447@qq.com>
2021-03-26 14:34:21 +0800

    2-6 完成hello world接口的编写

--- src/main/java/com/hailiang/wiki/config/WikiApplication.java
+++ src/main/java/com/hailiang/wiki/config/WikiApplication.java
@@ -1,11 +1,13 @@
-package com.hailiang.wiki;
+package com.hailiang.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.hailiang")
 @SpringBootApplication
 public class WikiApplication {
 
